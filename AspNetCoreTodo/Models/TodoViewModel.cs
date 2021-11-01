ca7629905ecf1cc22bc99535bbef7a6eb6658198
ramakrishnamuni <66634156+ramakrishnamuni@users.noreply.github.com>
2021-11-01 17:02:31 +0530

    intial code

---
+++ AspNetCoreTodo/Models/TodoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreTodo.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
